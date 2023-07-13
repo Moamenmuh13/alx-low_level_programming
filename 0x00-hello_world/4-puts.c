80e80f75598af9beb031ffda5278ce10d63b544f
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-07-13 23:51:29 +0300

    Fix Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,10 @@
 /**
 * Main - Entry point
 *
-* main fun print a specific message 
-*
 * Return: 0 on success
-*
 */
-int main(void){
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
