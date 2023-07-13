41dfa63ac6255f4ab4882eee7171c5a72e411826
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-07-13 23:44:05 +0300

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* Main Entry point
+*
+* main fun print a specific message 
+*
+* Return 0
+*
+*/
+int main(void){
+	puts("Programming is like building a multilingual puzzle");
+	retrun 0;
+}
