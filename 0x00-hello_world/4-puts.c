9fe05ccbdbe47ca94a4649775006af8d0055f780
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-07-13 23:47:57 +0300

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
-* Main Entry point
+* Main - Entry point
 *
 * main fun print a specific message 
 *
-* Return 0
+* Return: 0 on success
 *
 */
 int main(void){
-	puts("Programming is like building a multilingual puzzle");
-	retrun 0;
+	puts("\"Programming is like building a multilingual puzzle.");
+	return 0;
 }
