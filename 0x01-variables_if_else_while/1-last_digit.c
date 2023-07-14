0e34cfd7bc13a28d901cddfc576ea64ecccc33a7
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-07-14 22:45:21 +0300

    fix the last digit task

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
 	printf("Last digit of %d is %d", n, n % 10);
 
 	if (n % 10 > 5)
