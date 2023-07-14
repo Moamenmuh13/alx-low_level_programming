e83ef830222816e16d4c89e8f1f21500e76514c2
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-07-14 04:02:56 +0300

    Quote task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*This function serves as the entry point of the program.
+*It writes a specifc message to the standard error.
+*
+*Return: Always 1 (Error)
+*/
+int main(void)
+{
+write(2,"and that piece of art is useful\"- Dora Korpar, 2015-10-19\n",59);
+return (1);
+}
