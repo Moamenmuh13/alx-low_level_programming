83312179af6896bc02c539a8d3c7992648f7764a
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-07-18 23:56:42 +0300

    Print all minutes

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Print all minutes in the day
+ */
+
+void jack_bauer(void)
+{
+int hours, minute;
+
+for (hours = 0; hours <= 23; hours++)
+{
+for (minute = 0; minute <= 59; minute++)
+{
+_putchar((hours / 10) + '0');
+_putchar((hours % 10) + '0');
+_putchar(':');
+_putchar((minute / 10) + '0');
+_putchar((minute % 10) + '0');
+_putchar('\n');
+}
+}
+}
