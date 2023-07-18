9a911ace493cd34bb8c89c7db90ac7c989bd0521
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-07-18 21:50:44 +0300

    Print all alphabets 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - print all alphabet 10 times in small-case
+ */
+void print_alphabet_x10(void)
+{
+char letter;
+int count = 0;
+for (count = 0; count <= 10; count++)
+{
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
+
+_putchar('\n');
+}
+}
