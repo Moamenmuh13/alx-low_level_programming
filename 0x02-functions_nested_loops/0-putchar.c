4e57f428a1460b57c892c4e778dc487153aa3dee
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-07-18 08:52:36 -0700

    inital for task o

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <main.h>
 /**
  * main - Entry point
  *
@@ -6,7 +6,16 @@
  */
 int main(int argc, char const *argv[])
 {
-    printf("_putchar");
+    _putchar("_");
+    _putchar("p");
+    _putchar("u");
+    _putchar("t");
+    _putchar("c");
+    _putchar("h");
+    _putchar("a");
+    _putchar("r");
+    _putchar("\n");
+   
     
     return 0;
 }
