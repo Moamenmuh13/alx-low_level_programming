e98fbad7777bcdc194ae7c862282b4f10111e043
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-07-19 00:51:33 +0300

    last task

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_to_98 - print all number to reach 98
+ *
+ * @number: the input pramater
+ */
+void print_to_98(int number)
+{
+if (number >= 98)
+{
+while (number > 98)
+printf("%d, ", number--);
+
+printf("%d\n, ", number);
+}
+else
+{
+while (number < 98)
+printf("%d, ", number++);
+
+printf("%d\n", number);
+}
+}
