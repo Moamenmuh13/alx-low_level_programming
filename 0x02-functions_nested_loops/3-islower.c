fced7468bcbcf8d33c5feb2aeb351007088a93e0
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-07-18 22:40:26 +0300

    fix task num 4

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,18 @@
 #include "main.h"
 /**
- * is_lower - print alphas in small-cases
+ * _islower - print alphas in small-cases
+ * 
+ * @c: the input character to check on it
+ * 
+ * Return: If it return 1 it will be small-char else will be capital
+ * 
  */
 int _islower(int c)
 {
-    if (c >= 'a' && c<= 'z')
-    {
-        return(1);
-    }else{
-        return(0);
-    }
+if (c >= 'a' && c<= 'z')
+{
+return(1);
+}else{
+return(0);
+}
 }
