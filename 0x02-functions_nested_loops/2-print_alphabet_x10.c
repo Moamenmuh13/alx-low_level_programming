cb454cc7eabdb1156f4204d92bd1b88c9a616b90
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-07-18 22:02:21 +0300

    Print all alphabets 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet - print all alphabet 10 times in small-case
+ * print_alphabet_x10 - print all alphabet 10 times in small-case
  */
 void print_alphabet_x10(void)
 {
