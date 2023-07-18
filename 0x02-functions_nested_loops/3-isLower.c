70d222ed0049886312be1e5a05c68d7ad3d3b885
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-07-18 22:12:54 +0300

    isLower task

---
+++ 0x02-functions_nested_loops/3-isLower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * is_lower - print all alphabet 10 times in small-case
+ */
+int _isLower(int c)
+{
+    if (c >= 'a' && c<= 'z')
+    {
+        return(1);
+    }else{
+        return(0);
+    }
+    
+
+}
