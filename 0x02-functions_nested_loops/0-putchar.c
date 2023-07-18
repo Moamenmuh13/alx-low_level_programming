74471fd7c893073c6d6c0e31068c29ea17db5cba
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-07-18 19:46:29 +0300

    fix task num 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,21 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-int main(int argc, char const *argv[])
+int main(void)
 {
-    printf("_putchar");
-    
-    return 0;
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
+
+    return (0);
+
 }
