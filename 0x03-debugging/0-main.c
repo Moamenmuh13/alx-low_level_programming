eae42d729f00d2cfdedc8ff60c44ee6695d4e1ff
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-07-20 15:33:51 +0300

    Debugging task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,25 +2,15 @@
 /**
  * main - Entry point
  *
- * This function serves as the entry point of the program.
- * It assigns a random number to the variable n and prints
- * whether the number is positive, zero, or negative.
+ * This function to test if i added 0 as input 
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-int n;
+int i = 0;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
+positive_or_negative(i);
 
-if (n > 0)
-printf("%d is positive\n", n);
-else if (n == 0)
-printf("%d is zero\n", n);
-else
-printf("%d is negative\n", n);
 return (0);
 }
-
