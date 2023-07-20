a9f985cb2f64eb96c81bedee51f9371a461d482e
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-07-21 01:36:36 +0300

    Print lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,16 +7,8 @@
 void print_line(int n)
 {
 char line;
-if (n == 0)
-{
-_putchar('\n');
-}
-else 
-{
-for ( line = '0'; line <= n; line++)
-{
+for (line = '0'; line < n; line++)
 _putchar('_');
-}
+
 _putchar('\n');
-}
 }
