a328074f2756a52f8208beb1023c599f87e4cbbb
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-07-21 00:39:58 +0300

    remove 2 and 4 from loop

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,10 +5,10 @@
 
 void print_numbers(void)
 {
-int number;
+    int number;
 
-for (number = 0; number <= 9; number++)
-_putchar(number + '0');
+    for (number = 0; number <= 9; number++)
+        _putchar(number + '0');
 
-_putchar('\n');
+    _putchar('\n');
 }
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_numbers - print all numbers from 0 to 9 followed by a new line except 2 and 4
+ */
+
+void print_numbers(void)
+{
+int number;
+
+for (number = 0; number <= 9; number++)
+{
+if (number == 2 || number == 4)
+{
+continue;
+}
+else
+{
+_putchar(number + '0');
+}
+}
+
+_putchar('\n');
+}
