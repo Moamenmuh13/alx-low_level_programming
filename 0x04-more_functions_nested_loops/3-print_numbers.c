9accde4efb05ed18fcacf39561332f77d31f83f9
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-07-21 00:04:55 +0300

    Print num from 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,9 +8,8 @@ void print_numbers(void)
 {
 int number;
 
-for ( number = '0'; number <= '9'; number++)
-_putchar(number);
+for ( number = 0; number <= 9; number++)
+_putchar(number + '0');
 
 _putchar('\n');
-
 }
