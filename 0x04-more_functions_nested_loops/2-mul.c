89c96b5fa8278051cf8b90c859a300776bf7d317
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-07-20 23:46:20 +0300

    multiple two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isdigit - get all digits only
+ * @a: parameter number 1
+ * @b: parameter number
+ * Return: result of multiple of two numbers
+ */
+
+int mul(int a, int b)
+{
+int result;
+result = a * b;
+return (result);
+}
