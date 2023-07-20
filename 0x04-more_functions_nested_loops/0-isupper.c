3e589e1ba146275b4211fc99065651b283bc5877
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-07-20 23:08:40 +0300

    fix task num 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,15 +4,15 @@
  *
  * @c: parameter to check if letter is cap or small
  *
- * Return: 1 for uppercase, 0 for smallcase 
+ * Return: 1 for uppercase, 0 for smallcase
  */
 
 int _isupper(int c)
 {
-if (c >= 'A' && c <= 'Z')
-{
-return (1);
-}
-else
-return (0);
+    if (c >= 'A' && c <= 'Z')
+    {
+        return (1);
+    }
+    else
+        return (0);
 }
