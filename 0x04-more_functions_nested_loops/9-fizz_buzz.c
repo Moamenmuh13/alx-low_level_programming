e0c2fbed098a104603b16d9d8b94428ea1cb6302
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-07-21 02:48:09 +0300

    Fix all files

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,24 +1,33 @@
 #include <stdio.h>
 
 /**
-* main - entry point
-*      Description: print numbers from 1 to 100 
-*      number that mul of 3 print Fizz
-*      number that mul of 5 print Buzz
-*      number that mul of 3 and 5 print FizzBuzz
-* 
-* Return: Always 0 (Success)
-*/
+ * main - entry point
+ *      Description: print numbers from 1 to 100
+ *      number that mul of 3 print Fizz
+ *      number that mul of 5 print Buzz
+ *      number that mul of 3 and 5 print FizzBuzz
+ *
+ * Return: Always 0 (Success)
+ */
 
-int main(void) {
-for (int i = 1; i <= 100; i++) {
-if (i % 3 == 0 && i % 5 == 0) {
+int main(void)
+{
+for (int i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+{
 printf("FizzBuzz ");
-} else if (i % 3 == 0) {
+}
+else if (i % 3 == 0)
+{
 printf("Fizz ");
-} else if (i % 5 == 0) {
+}
+else if (i % 5 == 0)
+{
 printf("Buzz ");
-} else {
+}
+else
+{
 printf("%d ", i);
 }
 }
