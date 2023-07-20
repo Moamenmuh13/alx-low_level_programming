d5caefa242e33a2377a4eec6f3251111dc05596b
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-07-21 01:47:19 +0300

    Print a square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_square - Print a square followed by a new line
+ * @size: parameter the size of square
+ */
+
+void print_square(int size)
+{
+int i, j;
+
+if (size <= 0)
+_putchar('\n');
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < i; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
