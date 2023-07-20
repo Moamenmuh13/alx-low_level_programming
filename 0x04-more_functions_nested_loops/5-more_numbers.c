587481e44fb54984d98fcc8f47ce01af18feb9de
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-07-21 01:19:21 +0300

    Print loop 10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,18 +5,18 @@
 
 void more_numbers(void)
 {
-int count, number;
+    int count, number;
 
-for (count = 1; count <= 10; count++)
-{
-for (number = 0; number <= 14; number++)
-{
-if (number >= 10)
-_putchar('1');
+    for (count = 1; count <= 10; count++)
+    {
+        for (number = 0; number <= 14; number++)
+        {
+            if (number >= 10)
+                _putchar('1');
 
-_putchar((number % 10) + '0');
-}
+            _putchar((number % 10) + '0');
+        }
 
-_putchar('\n');
-}
+        _putchar('\n');
+    }
 }
