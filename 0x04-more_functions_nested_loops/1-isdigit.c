dccc2c1bdd612956d4081e3d6518426211914326
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-07-20 23:37:21 +0300

    Check for digit only

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isdigit - get all digits only
+ * @c: parameter to check if digit or otherwise
+ *
+ * Return: 1 for digit, 0 for char
+ */
+
+int _isdigit(int c)
+{
+if (c >= 0 && c <= 9)
+return (1);
+else
+return (0);
+}
