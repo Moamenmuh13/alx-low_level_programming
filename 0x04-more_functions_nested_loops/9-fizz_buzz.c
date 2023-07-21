2cfe54fd9ec92cfd9dbdd59bcf4b786b3f64f9b7
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-07-21 03:10:15 +0300

    Fix all files

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,11 +17,11 @@ int num;
 for (num = 1; num <= 100; num++)
 {
 if (num % 3 == 0 && num % 5 == 0)
-printf("FizzBuzz");
+printf("FizzBuzz ");
 else if(num % 3 == 0)
-printf("Fizz");
+printf("Fizz ");
 else if (num % 5 == 0)
-printf("Buzz");
+printf("Buzz ");
 else
 printf("%d", num);
 }
