f39f45c4dcc245fcc2e04bdc5b50fd6d43b75f08
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-07-21 03:04:10 +0300

    Fix all files

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -4,19 +4,22 @@
 * print_triangle - prints a triangle of '#' characters
 * @size: the size of the triangle
 */
+
 void print_triangle(int size)
 {
+int i, j, k;
+
 if (size <= 0)
 {
 _putchar('\n');
 return;
 }
 
-for (int row = 0; row < size; row++)
+for (i = 1; i < size; i++)
 {
-for (int space = 0; space < size - row - 1; space++)
+for (k = size - i; k >= 1; k--)
 _putchar(' ');
-for (int hash = 0; hash < row + 1; hash++)
+for (j = 1; j <= i + 1; j++)
 _putchar('#');
 
 _putchar('\n');
