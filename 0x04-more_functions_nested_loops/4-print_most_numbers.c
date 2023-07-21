b511a5fe596d89a929ac9039037ea419bc60a234
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-07-21 03:57:33 +0300

    Fix all files

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,6 @@
 /**
 * print_most_numbers - print all numbers from 0 to 9 followed by a new line except 2 and 4
 */
-
 void print_most_numbers(void)
 {
 int number;
@@ -14,6 +13,5 @@ continue;
 else
 _putchar(number + '0');
 }
-
 _putchar('\n');
 }
