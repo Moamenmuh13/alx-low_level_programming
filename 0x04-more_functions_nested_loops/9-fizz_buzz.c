c570b891c94c56ad13ede5505709700186a1ba19
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-07-21 04:20:10 +0300

    Fix all files

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,14 +16,14 @@ int num;
 
 for (num = 1; num <= 100; num++)
 {
-if (num % 3 == 0 && num % 5 == 0)
-printf("FizzBuzz");
-else if (num % 3 == 0)
+if (num % 3 == 0 && !(num % 5 == 0))
 printf("Fizz");
-else if (num % 5 == 0)
+else if (num % 5 == 0 && !(num % 3 == 0))
 printf("Buzz");
+else if (num % 3 == 0 && num % 5 == 0)
+printf("FizzBuzz");
 else
-printf("%d ", num);
+printf("%d", num);
 }
 printf("\n");
 
