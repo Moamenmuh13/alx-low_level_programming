e84a4f212765c2d63eabb9b1023f61bda2f4270f
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-07-21 04:14:46 +0300

    Fix all files

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,14 +1,15 @@
 #include "main.h"
 /**
- * print_most_numbers - print all numbers from 0 to 9 
- * except 2 and 4  followed by a new line
- * Return: void
- */
+* print_most_numbers - print all numbers from 0 to 9 
+* except 2 and 4  followed by a new line
+* Return: void
+*/
 void print_most_numbers(void)
 {
 int number;
 
-for (number = 0; number <= 9; number++) {
+for (number = 0; number <= 9; number++)
+{
 if (number == 2 || number == 4)
 continue;
 else
