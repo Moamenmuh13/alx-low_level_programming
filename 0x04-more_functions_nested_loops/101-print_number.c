4c76b2bb6e096f5fc99df939d81fef775d5dff0d
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-07-21 04:30:24 +0300

    advanced task num 2

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,14 +1,15 @@
 #include "main.h"
 /**
  * print_number - print number
+ * @n : Parameter for number
  */
 
 void print_number(int n)
 {
 if (n < 0)
 {
-    _putchar('-');
-    n = -n;
+_putchar('-');
+n = -n;
 }
 
 if (n / 10 != 0)
