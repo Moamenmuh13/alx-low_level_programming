d8aa2db9e04b4716e93fe1a14dd100924b489e3c
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-07-25 04:48:58 +0300

    task number 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * rev_string - Print the string in reverse
+ * @s: The string to print in rev
+ */
+void rev_string(char *s)
+{
+int a = 0;
+int b, i;
+char c;
+
+while (s[a] != '\0')
+a++;
+
+i = a - 1;
+
+for (b = 0; c >= 0 && b < c; i-- ,b++)
+{
+c = s[b];
+s[b] = s[i];
+s[i] = d;
+}
+}
