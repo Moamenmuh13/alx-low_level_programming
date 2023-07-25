f7824dd98776ee771a4b8457e232de88f91d2cee
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-07-25 05:12:19 +0300

    task number 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * puts2 - Print every other character of a string, starting with the first character.
+ * puts2 - Print every other character of a string.
  * @str: The string to print
  */
 void puts2(char *str)
