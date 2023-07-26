23a066a8783c9484454fa0bf4ef0a72c92826016
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-07-26 21:02:00 +0300

    task num 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,12 +1,22 @@
 #include "main.h"
 /**
- * reverse_array - Concatenates src to dest.
- * @a: The source string to be appended tp dest.
- * @n: The maximum number of bytes to be copied from src.
- * 
+ * reverse_array - reverse the content of an array
+ * @a: An array of integers
+ * @n: The number of elements in the array
  */
 
 void reverse_array(int *a, int n)
 {
+int *start = a;
+int *end = a + n - 1;
 
+while (start < end)
+{
+int temp = *start;
+*start = *end;
+*end = temp;
+
+start++;
+end--;
+}
 }
