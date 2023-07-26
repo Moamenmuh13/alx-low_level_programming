3d77e866eb5e0c278db580d345d65838706ed78c
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-07-26 20:57:32 +0300

    task num 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,13 +1,22 @@
 #include "main.h"
 /**
  * _strcmp - Concatenates src to dest.
- * @s1: The destination string.
- * @s2: The source string to be appended tp dest.
- * Return: A pointer to the resulting string dest.
- * 
+ * @s1: The first string to be compared
+ * @s2: The second string to be compared
+ * Return: A negative, zero, or positive integer depending on the comparison result.
  */
 
 int _strcmp(char *s1, char *s2)
 {
+while (*s1 != '\0' && *s2 != '\0')
+{
+if (*s1 != *s2) 
+{
+return *s1 - *s2;
+}
+s1++;
+s2++;
+}
 
+return *s1 - *s2;
 }
