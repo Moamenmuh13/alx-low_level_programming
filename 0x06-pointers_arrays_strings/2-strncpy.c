24056edc79de3ac754dced3ddfb13dd33dd210e0
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-07-26 20:44:49 +0300

    task num 3

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,5 +10,16 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
+int i;
 
+for (i = 0; i < n && src[1] != '\0'; i++)
+dest[i] = src[i];
+
+while (i < n)
+{
+dest[i] = '\0';
+i++;
+
+}
+return dest;
 }
