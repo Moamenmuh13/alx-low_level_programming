fd3976f5eed049137443e0502b4e47ade0628ba2
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-07-26 21:29:36 +0300

    task num 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,11 +1,31 @@
 #include "main.h"
 /**
- * cap_string - Concatenates src to dest.
+ * cap_string - Capitalizes all words
+ * @str: The input string.
  * Return: A pointer to the resulting string dest.
- * 
  */
 
-char *cap_string(char *)
+char *cap_string(char *str)
 {
+int i, x;
+char separators[] = " \t\n,;.!?\"(){}";
 
+while (*(str + i))
+{
+if (*(str + i) >= 'a' && *(str + i) <= 'z')
+{
+if (i == 0)
+*(str + i) = *(str + i) - ('a' - 'A');
+else
+for (x = 0; i <= 12; x++)
+{
+if (separators[x] == *(str + i - 1))
+*(str + i) = *(str + i) - ('a' - 'A');
+
+}
+}
+i++;
+}
+
+return (str);
 }
