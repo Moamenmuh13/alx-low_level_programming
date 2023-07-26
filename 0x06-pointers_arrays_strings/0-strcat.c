65d292f4e0b2008fa730c381455b75a037a48d2d
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-07-26 18:44:52 +0300

    task num 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * _strcat - Concatenates src to dest.
+ * *_strcat - Concatenates src to dest.
  * @dest: The destination string.
- * @src: The source string to be appended tp dest
+ * @src: The source string to be appended tp dest.
  * 
  * Retrun: A pointer to the resulting string dest.
 */
@@ -15,7 +15,7 @@ char *dest_end = dest;
 while (*dest_end != '\0')
 dest_end++;
 
-while (*src)
+while (*src != '\0')
 {
 *dest_end = *src;
 dest_end++;
