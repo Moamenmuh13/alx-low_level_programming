b72eee88cb332c1d2e333e48836379a70e97150b
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-07-26 20:46:58 +0300

    task num 3

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,7 +5,6 @@
  * @src: The source string to be appended tp dest.
  * @n: The maximum number of bytes to be copied from src.
  * Return: A pointer to the resulting string dest.
- * 
  */
 
 char *_strncpy(char *dest, char *src, int n)
@@ -21,5 +20,5 @@ dest[i] = '\0';
 i++;
 
 }
-return dest;
+return (dest);
 }
