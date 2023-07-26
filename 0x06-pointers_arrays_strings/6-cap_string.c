c9902c7e1188327aa9c94b1ec00bd3b646e57069
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-07-26 21:32:46 +0300

    task num 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,23 +10,22 @@ char *cap_string(char *str)
 int i, x;
 char separators[] = " \t\n,;.!?\"(){}";
 
-    while (*(str + i))
-    {
-        if (*(str + i) >= 'a' && *(str + i) <= 'z')
-        {
-            if (i == 0)
-                *(str + i) = *(str + i) - ('a' - 'A');
-            else
-            {
-                for (x = 0; x <= 12; x++) // Change i to x here
-                {
-                    if (separators[x] == *(str + i - 1))
-                        *(str + i) = *(str + i) - ('a' - 'A');
-                }
-            }
-        }
-        i++;
-    }
-
-    return (str);
+while (*(str + i))
+{
+if (*(str + i) >= 'a' && *(str + i) <= 'z')
+{
+if (i == 0)
+*(str + i) = *(str + i) - ('a' - 'A');
+else
+{
+for (x = 0; x <= 12; x++) // Change i to x here
+{
+if (separators[x] == *(str + i - 1))
+*(str + i) = *(str + i) - ('a' - 'A');
+}
+}
+}
+i++;
+}
+return (str);
 }
