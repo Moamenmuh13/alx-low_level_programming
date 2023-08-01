a8e8c4dde636694cc3051ed99bccc774d6cbfc91
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-08-01 05:50:42 +0300

    task num 8

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_diagsums - chessboard
+ * @a: pointer to the chess pieces
+ * @size: pointer to the chess pieces
+*/
+
+void print_diagsums(int *a, int size)
+{
+int i, sum1 = 0, sum2 = 0;
+
+for (i = 0; i < size; i++)
+{
+sum1 += *(a + i * size + i);
+sum2 += *(a + i * size + (size - i - 1));
+}
+printf("%d, %d\n", sum1, sum2);
+}
