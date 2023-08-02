64f63fb5bb3c3ca4cd70c3eb032e904fb8be3da2
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-08-02 19:36:04 +0300

    fix task num 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,13 +1,34 @@
 #include "main.h"
 /**
- * 
- * 
- * 
- * 
- * 
- * 
+ * is_divisible - helper func to check if n is divisible by div
+ * @n: The number to be checked
+ * @div: The divsor to check agianst n
+ * Return 1 if n is divisible to by div, otherwise 0.
+*/
+int is_divisible(int n, int div)
+{
+if (div * div > n)
+return(0);
+
+if (n % div == 0)
+return (1);
+
+return (is_divisible(n, div + 1));
+}
+
+/**
+ * is_divisible - helper func to check if n is divisible by div
+ * @n: The number to be checked
+ * Return 1 if n is divisible to by div, otherwise 0.
 */
 int is_prime_number(int n)
 {
-    
+if (n <= 1 || n % 2 == 0)
+return (0);
+
+if (n == 2)
+return (1);
+
+return (!is_divisible(n, 3));
+
 }
