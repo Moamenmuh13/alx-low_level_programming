d7da4cdbd40c54d354c2bd02d77d8ef51b6a7a79
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-08-02 19:43:23 +0300

    fix task num 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,26 +2,18 @@
 /**
  * _sqrt_helper - Helper func to calculate the square root recursively.
  * @n: The number for which the square root is to be calculated
- * @low: the lower bound of the range.
- * @high: the upper bound of the range
+ * @guess: the current guess for the square root.
  * Return: returns the natural square root of a number.
 */
-int _sqrt_helper(int n, int low, int high)
+int _sqrt_helper(int n, int guess)
 {
-int mid, mid_squared;
+if (guess * guess == n)
+return (guess);
 
-if (low > high)
+if (guess * guess > n)
 return (-1);
 
-mid = (low + high) / 2;
-mid_squared = mid *mid;
-
-if (mid_squared == n)
-return (mid);
-else if (mid_squared < n)
-return (_sqrt_helper(n, mid + 1, high));
-else
-return (_sqrt_helper(n, low, mid - 1));
+return (_sqrt_helper(n, guess + 1));
 }
 
 /**
@@ -31,8 +23,8 @@ return (_sqrt_helper(n, low, mid - 1));
 */
 int _sqrt_recursion(int n)
 {
-if (n == 0)
-return (0);
+if (n < 0)
+return (-1);
 
-return (_sqrt_helper(n, 0, n));
+return (_sqrt_helper(n, 0));
 }
