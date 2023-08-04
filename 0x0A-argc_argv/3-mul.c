d8beb219fba00de7ed27902cefef6d00b39b3410
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-08-05 02:07:57 +0300

    fix  task num 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,14 +14,14 @@ int num1, num2;
 
 if (argc < 3)
 {
-printf("Error\n")
+printf("Error\n");
 return (1);
 }
 
 num1 = atoi(argv[1]);
 num2 = atoi(argv[2]);
 
-printf("%d\n", num1 *y);
+printf("%d\n", num1 * num2);
 
 return (0);
 }
