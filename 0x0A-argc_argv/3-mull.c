fad10425edd428d83077f2f804d21d1bd6e6352d
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-08-05 01:43:49 +0300

    task num 3

---
+++ 0x0A-argc_argv/3-mull.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ * @argc: The number of argument passed to the program
+ * @argv: an array of strings containing the argument
+ * Return: 0 on success, 1 if the program dosen't receive two arguments.
+*/
+
+int main(int argc, char *argv[])
+{
+int num1, num2, result;
+
+if (argc != 3)
+{
+printf("Error\n")
+return (1);
+}
+
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+
+result = num1 *num2;
+printf("%d\n", result);
+
+
+return (0);
+}
