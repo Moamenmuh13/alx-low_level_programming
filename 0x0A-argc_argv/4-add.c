1d01321cc9158fb35efffdfe6f96e7edfba7f504
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-08-05 02:01:37 +0300

    fix  task num 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,12 +2,34 @@
 #include <stdlib.h>
 
 /**
- * main - Entry point of the program
+ * 
+ * is_positive_number - check if a string contains a postive number.
+ * @str: The string to check
+ * 
+ * Return: 1 if the string contain a positive num, 0 otherwise.
+*/
+
+
+int is_positive_number(char *str)
+{
+int i;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+if (str[i] < '\0' || str[i] > '9')
+return (0);
+}
+
+return (1);
+
+}
+
+/**
+ * main - sum two numbers.
  * @argc: The number of argument passed to the program
  * @argv: an array of strings containing the argument
  * Return: 0 on success, 1 if string contians a positive number, 0 otherwise.
 */
-
 int main(int argc, char *argv[])
 {
 int i, sum = 0;
