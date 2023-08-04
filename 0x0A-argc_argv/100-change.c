a51b4ebc8ecc5cfb976b8fab72916d7ce87b2f60
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-08-05 02:27:38 +0300

    reformat advanced task

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,48 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-/**
- * min_coins - calculate the min number of coins needed
- * @cents: The amount of cents to make change for
- * Return: the min number of coins
-*/
-
-int min_coins(int cents)
-{
-int coins = 0;
-
-if (cents <= 0)
-return (0);
-
-while (cents >= 25)
-{
-cents -= 25;
-coins++;
-}
-
-while (cents >= 10)
-{
-cents -= 10;
-coins++;
-}
-
-while (coins >= 5)
-{
-cents -= 5;
-coins++;
-}
-
-while (coins >= 2)
-{
-cents -= 2;
-coins++;
-}
-
-coins += cents;
-
-return (coins);
-}
-
 /**
  * main - Entry point of the program
  * @argc: The number of argument passed to the program
@@ -50,13 +7,13 @@ return (coins);
  * Return: always 0
 */
 
-int main(int argc, char *argv[])
+int main(int argc, char const *argv[])
 {
-int cents, coins;
+int cents, coins = 0;
 
 if (argc != 2)
 {
-printf("Error\n");
+printf("Error\n")
 return (1);
 }
 
@@ -68,10 +25,23 @@ printf("0\n");
 return (0);
 }
 
-coins = min_coins(cents);
-printf("%d\n", coins);
+while (cents > 0)
+{
+if (cents >= 25)
+cents -= 25;
+else if (cents >= 10)
+cents -= 10;
+else if (cents >= 5)
+cents -= 5;
+else if (cents >= 2)
+cents -= 2;
+else 
+cents -= 1;
 
-return (0);
+coins++;
+}
+
+printf("%d\n", coins);
 
 return (0);
 }
