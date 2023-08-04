65b856610e1f7cad07a65577e9961335e0bbc853
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-08-05 01:33:46 +0300

    task num 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ * @argc: The number of argument passed to the program
+ * @argv: an array of strings containing the argument
+ * Return: always 0
+*/
+
+
+int main(int argc, char *argv[])
+{
+(void)argc;
+
+printf("%s\n", argv[0]);
+
+return (0);
+}
