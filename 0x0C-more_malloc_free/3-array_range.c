02c99b5a51f7c91ab1c8cbc2fecd2c707c72142a
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-08-10 19:40:14 +0300

    task num 3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,14 +1,32 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * _calloc - Create an array of chars
+ * array_range - Create an array of integers
  * @min: size of array
  * @max: the char to init the array with
  * Return: int
 */
 
-int *array_range(int min, int max);
+int *array_range(int min, int max)
 {
+int *arr;
+int size;
 
-return (int);
+if (min > max)
+return (NULL);
+
+size = max - min + 1;
+
+arr = malloc(sizeof(int) * size);
+
+if (arr == NULL)
+return (NULL);
+
+for (i = 0; i < size; i++)
+{
+arr[i] = min;
+min++;
+}
+
+return (arr);
 }
