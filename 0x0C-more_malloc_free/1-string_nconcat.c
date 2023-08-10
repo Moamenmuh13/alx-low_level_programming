c7e249e85ba3d90cf90b28a05af2a6f7ce93dd38
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-08-10 16:06:08 +0300

    task num 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,15 +1,39 @@
 #include "main.h"
 #include <stdlib.h>
+#include <string.h>
 /**
  * string_nconcat - Create an array of chars
- * @s1: size of array
- * @s2: the char to init the array with
- * @n: the char to init the array with
- * Return: successful return the array
+ * @s1: The first string
+ * @s2: The second string
+ * @n: The maximum number of bytes from s2
+ * Return : A pointer to the newly allocated concatenated string.
 */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+char *concatenated;
+unsigned int len_s1, len_s2, total_length;
 
-return (char);
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+
+len_s1= strlen(s1);
+len_s2= strlen(s2);
+
+if (n >= len_s2)
+n = len_s2;
+
+total_length = len_s1 + n + 1;
+
+concatenated = malloc(total_length);
+
+if (concatenated == NULL)
+return (NULL); 
+
+strncpy(concatenated, s1, len_s1);
+strncpy(concatenated, len_s1, s2, n);
+
+return (concatenated);
 }
