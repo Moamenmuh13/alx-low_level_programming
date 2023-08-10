712b459b9425ed20c355effcd4e37666b31a3da8
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-08-10 19:55:03 +0300

    fix task num 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,7 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
-
 /**
  * string_nconcat - Create an array of chars
  * @s1: The first string
@@ -13,7 +11,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *concatenated;
-unsigned int len_s1, len_s2, total_length;
+unsigned int len_s1, len_s2, i, j;
 
 if (s1 == NULL)
 s1 = "";
@@ -21,21 +19,28 @@ s1 = "";
 if (s2 == NULL)
 s2 = "";
 
-len_s1 = strlen(s1);
-len_s2 = strlen(s2);
+while (s1[len_s1] != '\0')
+len_s1++;
+
+while (s2[len_s2] != '\0')
+len_s2++;
 
 if (n >= len_s2)
 n = len_s2;
 
-total_length = len_s1 + n + 1;
-
-concatenated = malloc(total_length);
+concatenated = malloc(sizeof(char) * (len_s1 + n + 1));
 
 if (concatenated == NULL)
 return (NULL);
 
-strncpy(concatenated, s1, len_s1);
-strncpy(concatenated + len_s1, s2, n);
+for (i = 0; i < len_s1; i++)
+concatenated[i] = s1[i];
+
+for (j = 0; j < len_s2; j++)
+concatenated[j] = s2[j];
+
+
+concatenated[i + j] = '\0';
 
 return (concatenated);
 }
