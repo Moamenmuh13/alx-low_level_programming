9a8b73e432dbece711a45fb2477ea579d48510f6
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-08-10 16:09:05 +0300

    task num 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,12 +1,13 @@
 #include "main.h"
 #include <stdlib.h>
 #include <string.h>
+
 /**
  * string_nconcat - Create an array of chars
  * @s1: The first string
  * @s2: The second string
  * @n: The maximum number of bytes from s2
- * Return : A pointer to the newly allocated concatenated string.
+ * Return: A pointer to the newly allocated concatenated string.
 */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
@@ -19,8 +20,8 @@ s1 = "";
 if (s2 == NULL)
 s2 = "";
 
-len_s1= strlen(s1);
-len_s2= strlen(s2);
+len_s1 = strlen(s1);
+len_s2 = strlen(s2);
 
 if (n >= len_s2)
 n = len_s2;
@@ -30,7 +31,7 @@ total_length = len_s1 + n + 1;
 concatenated = malloc(total_length);
 
 if (concatenated == NULL)
-return (NULL); 
+return (NULL);
 
 strncpy(concatenated, s1, len_s1);
 strncpy(concatenated, len_s1, s2, n);
--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,14 +1,20 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * _calloc - Create an array of chars
- * @nmemb: size of array
- * @size: the char to init the array with
- * Return: void
+ * _calloc - Allocates memory for an array and set memory to zero.
+ * @nmemb: The number of elements in the array
+ * @size: the size of each element in byts
+ * Return: a pointer to the allocated memory
 */
 
-void *_calloc(unsigned int nmemb, unsigned int size);
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
+void *ptr;
+unsigned int total_size;
 
+if (nmemb == 0 || size == 0)
+return (NULL);
+
+total_size = nmemb
 return (array);
 }
