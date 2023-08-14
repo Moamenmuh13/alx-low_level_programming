e2ac1601cd6d57d6eadbd1e22c37ff480c17f5a7
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-08-15 01:44:25 +0300

    create an object of dog class

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - init the dog class
+ * @d: a pointer of the class
+ * @name: pointer of the name of the dog
+ * @age: age of the dog
+ * @owner: pointer of the owner to the dog
+ * Return: void
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
