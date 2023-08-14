787d7beaa85622a438b8d09a87a1b14ced8e6939
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-08-15 02:16:06 +0300

    create a new type of dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,12 +19,6 @@ char *owner;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
-typedef struct dog dog_t
-{
-char *name;
-float age;
-char *owner;
-} dog_t;
-
+typedef struct dog dog_t;
 
 #endif
