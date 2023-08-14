68d5a01b37f4e192047dcf41f63371fd3f76f6ce
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-08-15 02:04:56 +0300

    task num 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,8 +10,17 @@ void print_dog(struct dog *d)
 {
 if (d != NULL)
 {
-printf("Name: %s\n", d->name ? d->name : "nill");
-printf("age: %s\n", d->age);
-printf("Owner: %s\n", d->owner ? d->owner : "nill");
+if (d->name != NULL)
+printf("Name: %s\n", d->name);
+else
+printf("nill \n");
+
+printf("Age: %f\n", d->age);
+
+if (d->owner != NULL)
+printf("A: %s\n", d->owner);
+else
+printf("nill \n");
+
 }
 }
