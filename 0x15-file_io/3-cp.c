10ccaf4155a89ee68573be7aaea46a450637ef91
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-09-05 03:59:17 +0300

    task num 3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,59 @@
+#include "main.h"
+
+/**
+ * main - Copies the content of one file to another.
+ * @argc: The number of command-line arguments.
+ * @argv: An array containing the command-line arguments.
+ *
+ * Return: 0 on success.
+ */
+int main(int argc, char *argv[])
+{
+int fd_from, fd_to;
+ssize_t bytes_read, bytes_written;
+char buffer[1024];
+
+if (argc != 3)
+{
+dprintf(2, "Usage: %s file_from file_to\n", argv[0]);
+exit(97);
+}
+
+fd_from = open(argv[1], O_RDONLY);
+if (fd_from == -1)
+{
+dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
+}
+
+fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+if (fd_to == -1)
+{
+dprintf(2, "Error: Can't write to %s\n", argv[2]);
+exit(99);
+}
+
+while ((bytes_read = read(fd_from, buffer, 1024)) > 0)
+{
+bytes_written = write(fd_to, buffer, bytes_read);
+if (bytes_written == -1 || bytes_written != bytes_read)
+{
+dprintf(2, "Error: Can't write to %s\n", argv[2]);
+exit(99);
+}
+}
+
+if (bytes_read == -1)
+{
+dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
+}
+
+if (close(fd_from) == -1 || close(fd_to) == -1)
+{
+dprintf(2, "Error: Can't close fd %d\n", (close(fd_from) == -1) ? fd_from : fd_to);
+exit(100);
+}
+
+return (0);
+}
