a515eec7cc6169f5462c048682881ce8d9affaba
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-09-05 04:06:19 +0300

    task num 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,59 +1,69 @@
 #include "main.h"
 
 /**
- * main - Copies the content of one file to another.
- * @argc: The number of command-line arguments.
- * @argv: An array containing the command-line arguments.
- *
- * Return: 0 on success.
+ * close_files - Close two file descriptors.
+ * @src_fd: Source file descriptor.
+ * @dest_fd: Destination file descriptor.
+ */
+void close_files(int src_fd, int dest_fd)
+{
+if (close(src_fd) == -1)
+{
+dprintf(2, "Error: Can't close fd %d\n", src_fd);
+exit(100);
+}
+if (close(dest_fd) == -1)
+{
+dprintf(2, "Error: Can't close fd %d\n", dest_fd);
+exit(100);
+}
+}
+
+/**
+ * main - Copy the content of a file to another file.
+ * @argc: Number of arguments.
+ * @argv: Array of argument strings.
+ * Return: 0 on success, appropriate error code on failure.
  */
 int main(int argc, char *argv[])
 {
-int fd_from, fd_to;
-ssize_t bytes_read, bytes_written;
-char buffer[1024];
-
 if (argc != 3)
 {
 dprintf(2, "Usage: %s file_from file_to\n", argv[0]);
 exit(97);
 }
+int src_fd, dest_fd;
+ssize_t n;
+char buffer[BUFFSIZE];
 
-fd_from = open(argv[1], O_RDONLY);
-if (fd_from == -1)
+src_fd = open(argv[1], O_RDONLY);
+if (src_fd == -1)
 {
 dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 exit(98);
 }
-
-fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-if (fd_to == -1)
+dest_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+if (dest_fd == -1)
 {
 dprintf(2, "Error: Can't write to %s\n", argv[2]);
+close(src_fd);
 exit(99);
 }
-
-while ((bytes_read = read(fd_from, buffer, 1024)) > 0)
+while ((n = read(src_fd, buffer, BUFFSIZE)) > 0)
 {
-bytes_written = write(fd_to, buffer, bytes_read);
-if (bytes_written == -1 || bytes_written != bytes_read)
+if (write(dest_fd, buffer, n) != n)
 {
 dprintf(2, "Error: Can't write to %s\n", argv[2]);
+close_files(src_fd, dest_fd);
 exit(99);
 }
 }
-
-if (bytes_read == -1)
+if (n == -1)
 {
 dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+close_files(src_fd, dest_fd);
 exit(98);
 }
-
-if (close(fd_from) == -1 || close(fd_to) == -1)
-{
-dprintf(2, "Error: Can't close fd %d\n", (close(fd_from) == -1) ? fd_from : fd_to);
-exit(100);
-}
-
+close_files(src_fd, dest_fd);
 return (0);
-}
+
