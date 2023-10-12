a9d1648b7770baad65e909fa6b2a1a2aa9ccf62f
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-10-12 20:13:20 +0300

    fix task 0 and done task 1

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,20 +1,21 @@
 #include "lists.h"
 
 /**
- * dlistint_len - Returns the number of elements in a doubly linked list.
+ * print_dlistint - Prints all the elements of a dlistint_t list.
  * @h: Pointer to the head of the list.
  *
  * Return: The number of elements in the list.
  */
 
-size_t dlistint_len(const dlistint_t *h)
+size_t print_dlistint(const dlistint_t *h)
 {
 	size_t count = 0;
 
 	while (h != NULL)
 	{
-		count++;
+		printf("%d\n", h->n);
 		h = h->next;
+		count++;
 	}
 
 	return (count);
