cc3430a94f54f345d60bb053e86c08effbddfacc
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-10-12 20:26:41 +0300

    fix task 3

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - Returns the number of elements in a doubly linked list.
+ * @head: A pointer to a pointer to the head of the list
+ * Return: The number of elements in the list.
+ */
+
+
+void free_dlistint(dlistint_t *head)
+{
+    
+}
