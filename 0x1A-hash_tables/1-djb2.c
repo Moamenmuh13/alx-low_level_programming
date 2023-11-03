bee013fcb4e135a4df3da06a12bfbd549e2724bc
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-11-04 01:08:16 +0200

    task 1

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,8 +1,8 @@
 #include "hash_tables.h"
 /**
- * hash_djb2 - Create a hash table.
- * @size: The size of the array in the hash table.
- * Return: A pointer to the newly created hashtable or NULL if an error occurs.
+ * hash_djb2 - Hash function implementing the djb2 algorithm.
+ * @str: The string to hash.
+ * Return: calculated hash.
  */
 
 unsigned long int hash_djb2(const unsigned char *str)
