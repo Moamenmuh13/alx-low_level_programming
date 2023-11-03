7d28c1cdec7ca3189b6052e79668c5f5079f8ca6
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-11-04 01:05:09 +0200

    task 1gcc -Wall -pedantic -Werror -Wextra -std=gnu89 main.c 1-djb2.c -o b

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,1 +1,18 @@
 #include "hash_tables.h"
+/**
+ * hash_djb2 - Create a hash table.
+ * @size: The size of the array in the hash table.
+ * Return: A pointer to the newly created hashtable or NULL if an error occurs.
+ */
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+
+	unsigned long int hash = 5381;
+	int c;
+
+	while ((c = *str++))
+		hash = ((hash << 5) + hash) + c;
+
+	return (hash);
+}
